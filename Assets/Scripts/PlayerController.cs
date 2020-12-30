2bd491db47d3ca5c8ff1fb709544a772fdcde145
Ne4m <ygz.a45@gmail.com>
2020-12-30 16:45:04 +0300

    Fırlatma Deneyimleri #1
    KEKW

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -20,8 +20,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(!isInWaypoint)
-        Movement();
+        //if(!isInWaypoint)
+        //Movement();
     }
 
     void Movement()
