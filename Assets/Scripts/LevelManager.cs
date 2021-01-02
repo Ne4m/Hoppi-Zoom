4a4e5ff39ef9d6c3d60c768638f14e0c8b92c16d
Runpace <hun.59@hotmail.com>
2021-01-02 03:48:37 +0300

    push

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -16,6 +16,8 @@ public class LevelManager : MonoBehaviour
     [SerializeField]
     private int point = 0;
 
+    private GameObject[] spawnedObjects = new GameObject[2];
+
     // Start is called before the first frame update
     void Start()
     {
@@ -51,15 +53,29 @@ public class LevelManager : MonoBehaviour
         point++;
 
 
-        //Debug.Log("Collision happened! Point: " + point);
-
-        /*if (last_CheckPoint != null && new_CheckPoint != last_CheckPoint) Destroy(last_CheckPoint);
-        if (new_CheckPoint != null) last_CheckPoint = new_CheckPoint;
+        
 
         new_CheckPoint = Instantiate(checkPoint_Prefab) as GameObject;
         new_CheckPoint.transform.position = transform.position;
-        new_CheckPoint.transform.position = new Vector3(0, new_CheckPoint.transform.position.y + 5.25f, 0);*/
+        new_CheckPoint.transform.position = new Vector3(0, new_CheckPoint.transform.position.y + 5.25f, 0);
 
+
+
+             if (spawnedObjects[0] == null)
+        {
+            spawnedObjects[0] = new_CheckPoint;
+        }
+        else if (spawnedObjects[1] == null)
+        {
+            spawnedObjects[1] = new_CheckPoint;
+        }
+        else if (spawnedObjects[0] != null)
+        {
+            Destroy(spawnedObjects[0]);
+
+            spawnedObjects[1] = spawnedObjects[0];
+            spawnedObjects[1] = null;
+        }
 
     }
 }
