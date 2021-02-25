488b0df64374be00ad3e16e1fb21e94a7e530a05
Ne4m <ygz.a45@gmail.com>
2021-02-25 19:40:36 +0300

    Main Menu Buttons Navigations + Character Menu Basic Logic

---
+++ Assets/CharacterMenuController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CharacterMenuController : MonoBehaviour
+{
+    private string[] playableCharacters = {"Default Guy", "Mahmut", "Kubat", "Ozan", "Samet", "Cavo"};
+    private string selectedCharacter;
+    private int currentCharacterIndex, maxCharacterIndex, minCharacterIndex;
+
+    [Header("Buttons")] 
+    [SerializeField] private Button swipeLeftButton;
+    [SerializeField] private Button swipeRightButton;
+    [SerializeField] private TMP_Text characterName;
+
+
+    void Awake()
+    {
+        currentCharacterIndex = getCurrentIndex();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        minCharacterIndex = 0;
+        maxCharacterIndex = playableCharacters.Length-1;
+
+        if(!(swipeLeftButton is null)) swipeLeftButton.onClick.AddListener(swipeLeftButtonClicked);
+        if(!(swipeRightButton is null)) swipeRightButton.onClick.AddListener(swipeRightButtonClicked);
+    }
+
+    private void swipeLeftButtonClicked()
+    {
+        if (currentCharacterIndex > minCharacterIndex)
+        {
+            currentCharacterIndex--;
+            updateCharacter(currentCharacterIndex);
+        }
+    }
+
+    private void swipeRightButtonClicked()
+    {
+        if (currentCharacterIndex < maxCharacterIndex)
+        {
+            currentCharacterIndex++;
+            updateCharacter(currentCharacterIndex);
+        }
+    }
+
+    private void updateCharacter(int index)
+    {
+        characterName.text = playableCharacters[index];
+    }
+    
+    private int getCurrentIndex()
+    {
+        // Player Prefs Stuff - Get Last Character And Return Index Based On That.. Implement Later.
+        return 0;
+    }
+}
--- Assets/Scripts/MainMenu_Controller.cs
+++ Assets/Scripts/MainMenu_Controller.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -10,28 +11,63 @@ public class MainMenu_Controller : MonoBehaviour
     private string playerModelSkin;
     private string playerArrowSkin;
 
-    [SerializeField] private Sprite audio_ON_IMG, audio_OFF_IMG;
-    [SerializeField] private Sprite music_ON_IMG, music_OFF_IMG;
+    [Header("Canvas UI Buttons")] 
+    [SerializeField] private Button characterMenuBack;
+    [SerializeField] private Button shopMenuBack;
+    
+    [Header("Canvas UI Objects")] 
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject characterMenu;
+    [SerializeField] private GameObject shopMenu;
 
+    [Header("Middle Buttons")] 
+    [SerializeField] private Button playButton;
+    [SerializeField] private Button characterButton;
+    [SerializeField] private Button shopButton;
+    
+    [Header("Down Menu IMG Resources")]
+    [SerializeField] private Sprite audioOnImg;
+    [SerializeField] private  Sprite audioOffImg;
+    [SerializeField] private  Sprite musicOnImg;
+    [SerializeField] private  Sprite musicOffImg;
+
+    [Header("Down Menu Buttons")] 
     [SerializeField] private Button audioButton;
     [SerializeField] private Button musicButton;
-    private bool audio_ON;
-    private bool music_ON;
-
+    [SerializeField] private Button shareButton;
+    [SerializeField] private Button infoButton;
     
+    
+    private bool audio_ON, music_ON;
+
+
     // Start is called before the first frame update
     void Start()
     {
         setPlayerSkin("modelSkin", playerModelSkin);
         setPlayerSkin("arrowSkin", playerArrowSkin);
 
+        // Canvas UI Elements Stuff
+        canvasBackMainMenu();
+        
+        if(!(characterMenuBack is null)) characterMenuBack.onClick.AddListener(canvasBackMainMenu);
+        if(!(shopMenuBack is null)) shopMenuBack.onClick.AddListener(canvasBackMainMenu);
+        
+        // Middle Menu Stuff
+        if(!(infoButton is null)) playButton.onClick.AddListener(playButtonClicked);
+        if(!(infoButton is null)) characterButton.onClick.AddListener(characterButtonClicked);
+        if(!(infoButton is null)) shopButton.onClick.AddListener(shopButtonClicked);
+        
+        
+        // Down Menu Stuff
         audio_ON = true;
         music_ON = true;
-        
+        if (!(audioButton is null)) audioButton.onClick.AddListener(changeAudioButton);
+        if (!(musicButton is null)) musicButton.onClick.AddListener(changeMusicButton);
+        if(!(shareButton is null)) shareButton.onClick.AddListener(shareButtonClicked);
+        if(!(infoButton is null)) infoButton.onClick.AddListener(infoButtonClicked);
         
 
-        if (!(audioButton is null)) audioButton.onClick.AddListener(changeAudio);
-        if (!(musicButton is null)) musicButton.onClick.AddListener(changeMusic);
     }
 
     void Awake()
@@ -61,32 +97,61 @@ public class MainMenu_Controller : MonoBehaviour
         }
     }
 
-    private void changeAudio()
+    private void changeAudioButton()
     {
         audio_ON = !audio_ON;
 
         if (audio_ON)
         {
-            audioButton.image.sprite = audio_ON_IMG;
+            audioButton.image.sprite = audioOnImg;
         }
         else
         {
-            audioButton.image.sprite = audio_OFF_IMG;
+            audioButton.image.sprite = audioOffImg;
         }
+
     }
 
-    private void changeMusic()
+    private void changeMusicButton()
     {
         music_ON = !music_ON;
 
         if (music_ON)
         {
-            musicButton.image.sprite = music_ON_IMG;
+            musicButton.image.sprite = musicOnImg;
         }
         else
         {
-            musicButton.image.sprite = music_OFF_IMG;
+            musicButton.image.sprite = musicOffImg;
         }
+        
+    }
+
+    private void shareButtonClicked()
+    {
+        print("Clicked Share Button!\n");
+    }
+
+    private void infoButtonClicked()
+    {
+        print("Clicked Info Button!\n");
+    }
+
+    private void playButtonClicked()
+    {
+        changeScene();
+    }
+
+    private void characterButtonClicked()
+    {
+        print("Clicked Character Button!");
+        canvasChangeUI("character");
+    }
+
+    private void shopButtonClicked()
+    {
+        print("Clicked Shop Button!");
+        canvasChangeUI("shop");
     }
 
     private void setPlayerSkin(string type, string skinName)
@@ -102,11 +167,37 @@ public class MainMenu_Controller : MonoBehaviour
             // Change Canvas Stuff HERE
         }
     }
-    
-    
 
 
-    public void changeScene()
+
+    private void canvasChangeUI(string IN)
+    {
+        switch (IN)
+        {
+            case "main":
+                mainMenu.SetActive(true);
+                characterMenu.SetActive(false);
+                shopMenu.SetActive(false);
+                break;
+            case "character":
+                mainMenu.SetActive(false);
+                characterMenu.SetActive(true);
+                shopMenu.SetActive(false);
+                break;
+            case "shop":
+                mainMenu.SetActive(false);
+                characterMenu.SetActive(false);
+                shopMenu.SetActive(true);
+                break;
+        }
+    }
+
+    private void canvasBackMainMenu()
+    {
+        canvasChangeUI("main");
+    }
+
+    private void changeScene()
     {
         SceneManager.LoadScene("Game");
     }
